3d27a7e20155134fd6ad911894515ee2adc6560f
naomi-homma <hnm.n.1029@gmail.com>
2021-06-12 16:20:42 +0900

    add:lesson04実装

--- lesson04/index.js
+++ lesson04/index.js
@@ -4,19 +4,19 @@ const ulElement = document.getElementById("ul");
 const frag = document.createDocumentFragment();
 
 const itemList = [
-    { link : 'a1.html', text: 'a1', src: "/img/bookmark.png"},
-    { link : 'a2.html', text: 'a2', src: "/img/bookmark.png"}
-];
+    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
 
 itemList.forEach(function(item) {
     const liElement = document.createElement("li");
     const anchorElement = document.createElement("a");
     const imageElement = document.createElement("img");
-    anchorElement.href= item.link;
+    anchorElement.href= `/` + `${item.to}`;
     anchorElement.textContent = item.text;
-    imageElement.src = item.src;
+    imageElement.src = item.img;
+    imageElement.alt = item.alt;
     frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
-    console.dir(frag)
 })
 
 ulElement.appendChild(frag);
