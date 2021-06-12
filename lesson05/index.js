417fb0f72945a74f97b3d9f131e61d9f5e7d9df2
naomi-homma <hnm.n.1029@gmail.com>
2021-06-12 17:10:13 +0900

    add:開発環境構築

---
+++ lesson05/index.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const ulElement = document.getElementById("ul");
+const frag = document.createDocumentFragment();
+
+const itemList = [
+    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
+
+itemList.forEach(function(item) {
+    const liElement = document.createElement("li");
+    const anchorElement = document.createElement("a");
+    const imageElement = document.createElement("img");
+    anchorElement.href= `/` + `${item.to}`;
+    anchorElement.textContent = item.text;
+    imageElement.src = item.img;
+    imageElement.alt = item.alt;
+    frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+})
+
+ulElement.appendChild(frag);
