4c9be6bd5406a9936065925b82d126fc553852fa
naomi-homma <hnm.n.1029@gmail.com>
2021-06-12 15:43:08 +0900

    add:lesson03実装完了

--- lesson03/index.js
+++ lesson03/index.js
@@ -1,14 +1,23 @@
 "use strict";
 
 const ulElement = document.getElementById("ul");
-const liElement = document.createElement("li");
-const anchorElement = document.createElement("a");
-const imageElement = document.createElement("img");
+const frag = document.createDocumentFragment();
 
-anchorElement.textContent = "これです！";
-anchorElement.href= "1.html";
-imageElement.src = "bookmark.png";
-imageElement.alt = "ブックマーク";
+const itemList = [
+    { link : 'a1.html', text: 'a1', src: "/img/bookmark.png"},
+    { link : 'a2.html', text: 'a2', src: "/img/bookmark.png"}
+];
 
-ulElement.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+for(var i = 0, len = itemList.length; i < len; i++) {
+    //nodeを作成していく処理
+    let list = itemList[i];
+    const liElement = document.createElement("li");
+    const anchorElement = document.createElement("a");
+    const imageElement = document.createElement("img");
+    anchorElement.href= list.link;
+    anchorElement.textContent = list.text;
+    imageElement.src = list.src;
+    frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+}
 
+ulElement.appendChild(frag);
