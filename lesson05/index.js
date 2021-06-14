2bb613dac2fd74225207fff58d7101ef3990616b
naomi-homma <hnm.n.1029@gmail.com>
2021-06-14 23:14:51 +0900

    add:開発環境構築

--- lesson05/index.js
+++ lesson05/index.js
@@ -8,6 +8,9 @@ const itemList = [
     {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
 ]
 
+//DOMに挿入する以下を関数としてまとめて
+//Promisのthenにぶちこむわ
+
 itemList.forEach(function(item) {
     const liElement = document.createElement("li");
     const anchorElement = document.createElement("a");
