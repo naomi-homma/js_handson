f6badafbe457596e6587da3d4f89bc26bb6606b2
naomi-homma <hnm.n.1029@gmail.com>
2021-06-19 18:48:27 +0900

    add:開発環境構築

---
+++ lesson07/index.js
@@ -0,0 +1,33 @@
+"use strict";
+
+//実装方針
+
+
+function createListDom(itemList) {
+    const ulElement = document.getElementById("ul");
+    const frag = document.createDocumentFragment();
+
+    itemList.forEach(function(item) {
+        const liElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
+        const imageElement = document.createElement("img");
+        anchorElement.href= `/${item.to}`;
+        anchorElement.textContent = item.text;
+        imageElement.src = item.img;
+        imageElement.alt = item.alt;
+        frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+    });
+    ulElement.appendChild(frag);
+}
+
+const fetchData = new Promise((resolve) => {
+    const itemList = [
+        {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+        {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    ];
+    setTimeout(function () {
+        resolve(itemList);
+      }, 3000);
+});
+
+fetchData.then((itemList) => createListDom(itemList));
