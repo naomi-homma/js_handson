0ba10918af1e72953c0150e523d2958817513f70
naomi-homma <hnm.n.1029@gmail.com>
2021-06-19 13:03:05 +0900

    add:lesson05実装完了

--- lesson05/index.js
+++ lesson05/index.js
@@ -1,25 +1,28 @@
 "use strict";
 
-const ulElement = document.getElementById("ul");
-const frag = document.createDocumentFragment();
+function createListDom(itemList) {
+    const ulElement = document.getElementById("ul");
+    const frag = document.createDocumentFragment();
 
-const itemList = [
-    {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
-    {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
-]
+    itemList.forEach(function(item) {
+        const liElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
+        const imageElement = document.createElement("img");
+        anchorElement.href= `/${item.to}`;
+        anchorElement.textContent = item.text;
+        imageElement.src = item.img;
+        imageElement.alt = item.alt;
+        frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+    });
+    ulElement.appendChild(frag);
+}
 
-//DOMに挿入する以下を関数としてまとめて
-//Promisのthenにぶちこむわ
+const fetchData = new Promise((resolve) => {
+    const itemList = [
+        {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+        {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    ];
+    resolve(itemList);
+});
 
-itemList.forEach(function(item) {
-    const liElement = document.createElement("li");
-    const anchorElement = document.createElement("a");
-    const imageElement = document.createElement("img");
-    anchorElement.href= `/` + `${item.to}`;
-    anchorElement.textContent = item.text;
-    imageElement.src = item.img;
-    imageElement.alt = item.alt;
-    frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
-})
-
-ulElement.appendChild(frag);
+fetchData.then((itemList) => createListDom(itemList));
