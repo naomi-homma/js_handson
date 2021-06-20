6eafe119f497aeb3259c8aa2db283355eacc8d6e
naomi-homma <hnm.n.1029@gmail.com>
2021-06-20 12:32:01 +0900

    add:開発環境構築

---
+++ lesson09/index.js
@@ -0,0 +1,51 @@
+"use strict";
+
+function createListDom(itemList) {
+    const ulElement = document.getElementById("ul");
+    const frag = document.createDocumentFragment();
+
+    itemList.forEach(function(item) {
+        const liElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
+        const imageElement = document.createElement("img");
+        anchorElement.href= `/${item.to}`;
+        anchorElement.textContent = item.text;
+        imageElement.src = item.img;
+        imageElement.alt = item.alt;
+        frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+    });
+    ulElement.appendChild(frag);
+}
+
+function removeLoading() {
+    const loadingImage = document.querySelector(".js-loading");
+    loadingImage.classList.add("loaded");
+    // loadingImage.parentNode.removeChild(loadingImage);
+}
+
+async function fetchData() {
+    const itemList = [
+        {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+        {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    ];
+    setTimeout(function () {
+        resolve(itemList);
+      }, 3000);
+
+    const createListDom = await createListDom(itemList)
+    const removeLoading = await removeLoading()
+
+    await
+}
+
+const fetchData = new Promise((resolve) => {
+    const itemList = [
+        {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+        {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    ];
+    setTimeout(function () {
+        resolve(itemList);
+      }, 3000);
+});
+
+fetchData.then((itemList) => createListDom(itemList)).then(() => removeLoading());
