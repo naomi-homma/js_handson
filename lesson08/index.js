7d79a1132e5fe707a5fdf7f5d974dca126e8fb00
naomi-homma <hnm.n.1029@gmail.com>
2021-06-20 11:40:43 +0900

    add:reject実装

---
+++ lesson08/index.js
@@ -0,0 +1,36 @@
+"use strict";
+
+function createListDom(itemList) {
+    const ulElement = document.getElementById("ul");
+    const frag = document.createDocumentFragment();
+
+    itemList.forEach(function(item) {
+        const liElement = document.createElement("li");
+        const anchorElement = document.createElement("a");
+        const imageElement = document.createElement("img");
+        anchorElement.href= `/${item.to}`;
+        anchorElement.textContent = item.text;
+        imageElement.src = item.img;
+        imageElement.alt = item.alt;
+        frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+    });
+    ulElement.appendChild(frag);
+}
+
+function removeLoading() {
+    const loadingImage = document.querySelector(".js-loading");
+    loadingImage.classList.add("loaded");
+    // loadingImage.parentNode.removeChild(loadingImage);
+}
+
+const fetchData = new Promise((resolve, reject) => {
+    // const itemList = [
+    //     {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+    //     {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+    // ];
+    setTimeout(function () {
+        reject("データがありません！");
+      }, 3000);
+});
+
+fetchData.then(() => createListDom()).catch((e) => console.log(e));
