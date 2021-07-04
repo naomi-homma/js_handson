17cd379d7881cea31875e90958fca1d28a8f460c
naomi-homma <hnm.n.1029@gmail.com>
2021-07-04 17:09:28 +0900

    fix:Rename constant, change scope of ul element

--- lesson09/index.js
+++ lesson09/index.js
@@ -1,31 +1,30 @@
 "use strict";
 
-function createListDom(itemList) {
-    const ulElement = document.getElementById("ul");
-    const frag = document.createDocumentFragment();
+const ul = document.getElementById("ul");
 
+function createListDom(itemList) {
     itemList.forEach(function(item) {
-        const liElement = document.createElement("li");
-        const anchorElement = document.createElement("a");
-        const imageElement = document.createElement("img");
-        anchorElement.href= `/${item.to}`;
-        anchorElement.textContent = item.text;
-        imageElement.src = item.img;
-        imageElement.alt = item.alt;
-        frag.appendChild(liElement).appendChild(anchorElement).appendChild(imageElement)
+        const frag = document.createDocumentFragment();
+        const li = document.createElement("li");
+        const anchor = document.createElement("a");
+        const image = document.createElement("img");
+        anchor.href= `/${item.to}`;
+        anchor.textContent = item.text;
+        image.src = item.img;
+        image.alt = item.alt;
+        frag.appendChild(li).appendChild(anchor).appendChild(image)
     });
-    ulElement.appendChild(frag);
+    ul.appendChild(frag);
 }
 
 function createLoading() {
-    const ulElement = document.getElementById("ul");
-    const loadingImgWrapElement = document.createElement("div");
-    const img = document.createElement("img");
-    loadingImgWrapElement.classList.add("js-loading");
-    img.src = "./img/loading-circle.gif";
-    img.classList.add("loading-img");
-    loadingImgWrapElement.appendChild(img);
-    ulElement.before(loadingImgWrapElement);
+    const loadingImgWrap = document.createElement("div");
+    const image = document.createElement("img");
+    loadingImgWrap.classList.add("js-loading");
+    image.src = "./img/loading-circle.gif";
+    image.classList.add("loading-img");
+    loadingImgWrap.appendChild(image);
+    ul.before(loadingImgWrap);
 }
 
 function removeLoading() {
